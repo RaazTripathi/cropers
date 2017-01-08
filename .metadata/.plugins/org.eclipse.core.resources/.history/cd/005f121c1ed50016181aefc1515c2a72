c4d9fd4d3632cd936137342e8896d8ff905df95b
Raaz <raaz19.tripathi@gmail.com>
2017-01-08 03:11:00 +0000

    changed project conf

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/cd/005f121c1ed50016181aefc1515c2a72
@@ -0,0 +1,15 @@
+package com.croper.web;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+import com.croper.web.main.CroperWebAppApplication;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CroperWebAppApplication.class);
+	}
+
+}
---
+++ croper-pcm/src/main/java/com/croper/product/CrCategory.java
@@ -0,0 +1,103 @@
+package com.croper.product;
+
+import java.util.Set;
+
+public class CrCategory {
+	String id;
+	String name;
+	String description;
+	Set<CrProduct> products;
+	
+	
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the products
+	 */
+	public Set<CrProduct> getProducts() {
+		return products;
+	}
+	/**
+	 * @param products the products to set
+	 */
+	public void setProducts(Set<CrProduct> products) {
+		this.products = products;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((products == null) ? 0 : products.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CrCategory other = (CrCategory) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (products == null) {
+			if (other.products != null)
+				return false;
+		} else if (!products.equals(other.products))
+			return false;
+		return true;
+	}
+	
+
+}
